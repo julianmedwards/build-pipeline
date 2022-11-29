b5b3ef62e555099895693d186ea69c335388ed3a
julianmedwards <julian.michael.edwards@gmail.com>
2022-11-29 11:07:22 -0600

    Imported movieDB site, some basic setup

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+let gulp = require('gulp')
+
+// Minify CSS
+// gulp.task('minify', function () {
+//     console.log('Minifying')
+// })
+
+// Apply global prettier to HTML/JS
+
+// PostCSS
+
+// Optimize images
+
+// Create static folder
+
+// Run tests
